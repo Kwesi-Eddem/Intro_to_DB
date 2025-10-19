78b920e4f1edf6be0e92dcc3d2b98931d7b2dd75
addem <kwesi.eddem@gmail.com>
2025-10-19 22:20:26 +0000

    3rd edit task6

--- task_6.sql
+++ task_6.sql
@@ -5,8 +5,8 @@ VALUES(2,'Blessing Malik','bmalik@sandtech.comm','124 Happiness  Ave.')
 
 
 INSERT INTO customer(customer_id,customer_name,email,address)
-VALUES(3,'Obed Ehoneah','eobed@sandtech.com','125 Happiness Ave.')
+VALUES(3,'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave.')
 
 
 INSERT INTO customer(customer_id,customer_name,email,address)
-VALUES(4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness Ave.')
+VALUES(4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness  Ave.')
